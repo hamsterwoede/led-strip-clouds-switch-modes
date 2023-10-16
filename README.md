34abde18d9950d20ef2d9875bdbdadd88d72885e
Martin Kloos <hamsterwoede@gmail.com>
2023-10-16 16:06:49 +0200

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://hamsterwoede.github.io/led-strip-clouds-switch-modes/](https://hamsterwoede.github.io/led-strip-clouds-switch-modes/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/hamsterwoede/led-strip-clouds-switch-modes** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/hamsterwoede/led-strip-clouds-switch-modes** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,127 @@
+function ClappingClouds () {
+    if (input.soundLevel() > Ruisvloer) {
+        strip.setBrightness(100 * (input.soundLevel() / (255 - Ruisvloer)))
+        isBright = true
+    } else {
+        strip.setBrightness(0)
+        strip.easeBrightness()
+        isBright = false
+    }
+    strip.showColor(neopixel.hsl(KleurWaarde, 255, 50))
+}
+function Wolk (Lengte: number) {
+    if (WolkLengte > 0) {
+        strip.setPixelColor(0, neopixel.hsl(200, randint(0, 100), randint(0, 100)))
+        strip.show()
+        strip.shift(1)
+        WolkLengte += -1
+    } else {
+        WolkAan = false
+    }
+    basic.pause(500)
+}
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    State = 1
+})
+function RGBLed () {
+    if (!(isBright)) {
+        strip.setBrightness(100)
+        strip.easeBrightness()
+    }
+    isBright = true
+    if (KleurWaarde < 360) {
+        KleurWaarde += 1
+    } else {
+        KleurWaarde = 0
+    }
+    strip.setPixelColor(0, neopixel.hsl(KleurWaarde, 255, 50))
+    strip.show()
+    strip.shift(1)
+}
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # # .
+        `)
+    State = 0
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . . # # .
+        . # . . .
+        . # # # .
+        `)
+    State = 2
+})
+function BlauweLucht () {
+    if (KleurWaarde > 230) {
+        RuisFactor = RuisFactor + randint(-5, 0) / RuisSchaal
+    } else if (KleurWaarde < 190) {
+        RuisFactor = RuisFactor + randint(0, 5) / RuisSchaal
+    } else {
+        RuisFactor = RuisFactor + randint(-3, 3) / RuisSchaal
+    }
+    if (randint(0, 10) > 8) {
+        strip.setPixelColor(0, neopixel.hsl(200, 255, 50))
+    } else {
+        strip.setPixelColor(0, neopixel.hsl(Math.round(KleurWaarde + RuisFactor), 255, 50))
+    }
+    strip.show()
+    strip.shift(1)
+    basic.pause(500)
+}
+let WolkLengte = 0
+let KleurWaarde = 0
+let State = 0
+let strip: neopixel.Strip = null
+let WolkAan = false
+let isBright = false
+let RuisSchaal = 0
+let RuisFactor = 0
+let Ruisvloer = 0
+Ruisvloer = 80
+RuisFactor = 0
+RuisSchaal = 20
+isBright = true
+WolkAan = false
+strip = neopixel.create(DigitalPin.P0, 6, NeoPixelMode.RGB)
+State = 0
+KleurWaarde = 200
+let Snelheid = 10
+strip.showColor(neopixel.hsl(KleurWaarde, 255, 50))
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # . # .
+    . # . # .
+    . # # # .
+    `)
+basic.forever(function () {
+    if (State == 0) {
+        if (WolkAan) {
+            Wolk(WolkLengte)
+        } else {
+            BlauweLucht()
+            if (randint(0, 40) > 39) {
+                WolkLengte = randint(1, 10)
+                WolkAan = true
+            }
+        }
+    } else if (State == 1) {
+        ClappingClouds()
+    } else {
+        RGBLed()
+    }
+})
